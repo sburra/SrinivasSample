c2c1d704fd4c5b5126bcc9adf0fd79280fdf1a45
srinivas.burra <srinivas.burra@HDC4-D-8K5L6C2.dir.svc.accenture.com>
2018-04-09 19:45:59 +0530

    main class to find the bean names

---
+++ src/main/java/com/srini/examples/SpringBootDemoApplication.java
@@ -0,0 +1,25 @@
+package com.srini.examples;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+ 
+@SpringBootApplication
+public class SpringBootDemoApplication {
+ 
+    public static void main(String[] args)
+    {
+        ApplicationContext ctx = SpringApplication.run(SpringBootDemoApplication.class, args);
+ 
+        String[] beanNames = ctx.getBeanDefinitionNames();
+         
+        Arrays.sort(beanNames);
+         
+        for (String beanName : beanNames)
+        {
+            System.out.println(beanName);
+        }
+    }
+}
